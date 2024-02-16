22cd66427921055dac708cd620baf8daba96e3b1
BrunoWeinberger <bw672558@gmail.com>
2024-02-16 23:49:09 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# автоматическое форматирование: ctrl alt L# дублировать строку: ctrl d
+a = int(input("Введите первое число \'a' : \n"))
+b = int(input("Введите второе число \'b' : \n"))
+print("Вы ввели числа:", a, "&", b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a ** b = ", a ** b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
